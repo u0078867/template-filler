9846d0562aa523da74bbfcb21156bc9b365e4629
Davide Monari <davide.monari@kuleuven.be>
2018-03-20 14:35:42 +0100

    added folder as input path; added fmt_<format> section

--- README.md
+++ README.md
@@ -18,12 +18,30 @@ to build for Windows, for instance.
 
 # Usage
 
+##### **Specify template and data file:**
+
 ```
 filler template-file data-file output-file
 ```
 
+In this case, the template placeholder must contain properties of the object contained inside `data-file`.
+
 Example:
 
 ```
-filler sample_data/Patient_report_template_190218.html sample_data/data.json sample_data/out.html
+filler sample_data/Patient_report_template_190218.html sample_data/data1.json sample_data/out.html
+```
+
+##### **Specify template and data folder:**
+
+```
+filler template-file data-folder output-file
+```
+
+In this case, the template placeholder must contain `{{<file name>.<props>}}`, where `<props<` are properties of the object contained inside `<file-name>.json` that is inside  `data-folder`. If there is only one JSON file inside `data-folder`, you can also use the syntax at **Specify template and data file**.
+
+Example:
+
+```
+filler sample_data/Patient_report_template_190218.html sample_data sample_data/out.html
 ```
--- filler.js
+++ filler.js
@@ -2,6 +2,7 @@ let args = require('args');
 let fs = require('fs');
 let path = require('path');
 let mustache = require('mustache');
+let sprintf = require('sprintf-js').sprintf;
 
 
 path.join(__dirname, 'package.json'); // for pkg to embed assets
@@ -10,9 +11,47 @@ const flags = args.parse(process.argv);
 
 let argv = process.argv.slice(2);
 
+// pre-parse template
+let sections = {};
 let template = fs.readFileSync(argv[0], 'utf8');
-let data = JSON.parse(fs.readFileSync(argv[1], 'utf8'));
+let tokens = mustache.parse(template);
+for (let token of tokens) {
+  if (token[0] == '#') {
+    let section = token[1];
+    let s = section.split('_');
+    switch (s[0]) {
+      case 'fmt':
+        sections[section] = function() {
+          return function(placeholder, render) {
+            let value = render(placeholder);
+            let text = sprintf(s[1].replace(',', '.'), value);
+            return text;
+          }
+        }
+        break;
+    }
+  }
+}
+mustache.clearCache();
 
+// create data
+let data = Object.assign({}, sections);
+let dataPathStat = fs.lstatSync(argv[1]);
+if (dataPathStat.isFile()) {
+  data = Object.assign(data, JSON.parse(fs.readFileSync(argv[1], 'utf8')));
+} else if (dataPathStat.isDirectory()) {
+  let dataFolder = argv[1];
+  let files = fs.readdirSync(dataFolder).filter(e => path.extname(e).toLowerCase() === '.json');
+  if (files.length == 1) {
+    data = Object.assign(data, JSON.parse(fs.readFileSync(path.join(dataFolder, files[0]), 'utf8')));
+  }
+  for (let file of files) {
+    fileData = JSON.parse(fs.readFileSync(path.join(dataFolder, file), 'utf8'));
+    data[path.parse(file).name] = fileData;
+  }
+}
+
+// render template
 var output = mustache.render(template, data);
 mustache.tags = ['%7B%7B', '%7D%7D'];
 output = mustache.render(output, data);
